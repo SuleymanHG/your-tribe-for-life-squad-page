ad8cd4f629b7cc02a8e05705ab0d6e8682353a96
Anouarab2 <anouar.ait.brahim@hva.nl>
2025-09-18 21:51:36 +0200

    heading toevoegen #36
    extra: de darkmode toggle gebruikte nog de oude tijdelijk background color, dit heb ik aangepast en ik heb wat meer verschil gemaakt in hoogte tussen de kaartjes, zo volgt het het design

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,10 +15,18 @@ onMount(() => {
 })
 </script>
 
+<main>
+    <section class="heading">
+        <h1>Maak kennis met Hva FDND</h1>
+        <p>Frontend Design & Development aan de HvA. Hier leren we ontwerpen en bouwen voor het web. 
+            Ontmoet alle studenten op onze squadpage!</p>
+    </section>
 
-<div class="layout-cards">
-    {#each members as m}
-      <img src={`https://fdnd.directus.app/assets/${m.mugshot}?w=200&h=200&fit=cover`} alt={m.name} />
-    {/each}
-</div>
+    <div class="layout-cards">
+        {#each members as m}
+          <img src={`https://fdnd.directus.app/assets/${m.mugshot}?w=200&h=200&fit=cover`} alt={m.name} />
+        {/each}
+    </div>
+    
+</main>
 
